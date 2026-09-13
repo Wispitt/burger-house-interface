60a7539763cf3a412bc610a5372272dbda2e6a59
Wispitt <marianasilvagois11@gmail.com>
2026-09-13 08:08:12 -0300

    Organizando.

--- src/components/Title/index.jsx
+++ src/components/Title/index.jsx
@@ -1,18 +1,17 @@
-import { TitleWrapper, IconLogo, SnackBar } from './styles'
-
 import logoBurger from '../../assets/img/burger-icon.png';
+import { IconLogo, SnackBar, TitleWrapper } from './styles';
 
 export function Title() {
-    return (
-        <>
-            <TitleWrapper>
-                <IconLogo src={logoBurger} alt="logo" />
-                <h1>Burger House</h1>
-            </TitleWrapper>
+	return (
+		<>
+			<TitleWrapper>
+				<IconLogo src={logoBurger} alt="logo" />
+				<h1>Burger House</h1>
+			</TitleWrapper>
 
-            <SnackBar>
-                <span>SNACK BAR</span>
-            </SnackBar>
-        </>
-    )
+			<SnackBar>
+				<span>SNACK BAR</span>
+			</SnackBar>
+		</>
+	);
 }
